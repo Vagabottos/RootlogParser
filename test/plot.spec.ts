037929b1c7377e50f4c613d77b6c4b0a6e9a8581
chadfraser <chad.fraser@mail.mcgill.ca>
2020-12-09 07:54:32 -0500

    Add tests

--- test/plot.spec.ts
+++ test/plot.spec.ts
@@ -1,5 +1,7 @@
 import test from 'ava-ts';
 
+import { ActionSwapPlots, ActionTriggerPlot } from '../src/interfaces';
+import { parseConspiracyAction } from '../src/parsers';
 import { parsePlotAction } from '../src/action-parser';
 
 test('Correctly parses an action to attempt to expose a plot', t => {
@@ -32,4 +34,34 @@ test('Correctly parses an action to trigger a raid plot in a clearing with multi
 
   t.is(result.plot, 't_r');
   t.is(result.clearing, 11);
+});
+
+test('Correctly parses action to flip a plot', t => {
+
+  const result = parseConspiracyAction('t6^t_e') as ActionTriggerPlot;
+
+  t.is(result.clearing, 6);
+  t.is(result.plot, 't_e');
+});
+
+test('Correctly parses action to flip a plot in a clearing with multiple digits', t => {
+
+  const result = parseConspiracyAction('t11^t_e') as ActionTriggerPlot;
+
+  t.is(result.clearing, 11);
+  t.is(result.plot, 't_e');
+});
+
+test('Correctly parses action to swap plots', t => {
+
+  const result = parseConspiracyAction('t1<->t9') as ActionSwapPlots;
+
+  t.deepEqual(result.clearings, [1, 9]);
+});
+
+test('Correctly parses action to swap plots in clearings with multiple digits', t => {
+
+  const result = parseConspiracyAction('t10<->t11') as ActionSwapPlots;
+
+  t.deepEqual(result.clearings, [10, 11]);
 });
